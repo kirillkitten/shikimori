039a6a73b85dee1392c145b3734a9c987cdcaa26
Kirill Kitten <khudiakovk@gmail.com>
2022-01-07 14:21:06 +0300

    add top bar to MainActivity.kt

--- app/src/main/java/kirillkitten/shikimori/main/MainActivity.kt
+++ app/src/main/java/kirillkitten/shikimori/main/MainActivity.kt
@@ -4,35 +4,52 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import dagger.hilt.android.AndroidEntryPoint
+import kirillkitten.shikimori.R
 import kirillkitten.shikimori.data.Anime
 import kirillkitten.shikimori.ui.theme.ShikimoriTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    private val viewModel by viewModels<MainViewModel>()
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ShikimoriTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    FetchAnimes()
-                }
+                AnimeGridScreen()
             }
         }
     }
 
     @Composable
-    fun FetchAnimes() {
+    private fun AnimeGridScreen() {
+        Scaffold(
+            topBar = {
+                TopAppBar(title = {
+                    Text(text = getString(R.string.app_name))
+                })
+            },
+            content = {
+                AnimeGridScreenContent()
+            }
+        )
+    }
+
+    @Composable
+    private fun AnimeGridScreenContent() {
         val animes: List<Anime> by viewModel.animes.observeAsState(emptyList())
-        if (animes.isNotEmpty()) AnimeGrid(animes) else LoadingScreen()
+        if (animes.isNotEmpty()) {
+            AnimeGrid(animes)
+        } else {
+            LoadingScreen()
+        }
     }
 }
