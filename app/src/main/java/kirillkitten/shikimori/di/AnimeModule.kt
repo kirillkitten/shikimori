a250924f17d716ef55ff781722f4b377864f2b63
Kirill Kitten <khudiakovk@gmail.com>
2022-01-10 03:48:47 +0300

    bind AnimeRepository to its implementation instead of providing

--- app/src/main/java/kirillkitten/shikimori/di/AnimeModule.kt
+++ app/src/main/java/kirillkitten/shikimori/di/AnimeModule.kt
@@ -2,38 +2,55 @@ package kirillkitten.shikimori.di
 
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kirillkitten.shikimori.ANIME_PAGE_SIZE
+import kirillkitten.shikimori.BASE_URL
 import kirillkitten.shikimori.data.Anime
-import kirillkitten.shikimori.data.AnimePagingConfig
 import kirillkitten.shikimori.data.AnimePagingSource
 import kirillkitten.shikimori.data.AnimeRepository
 import kirillkitten.shikimori.data.AnimeRepositoryImpl
 import kirillkitten.shikimori.data.remote.AnimeApi
-import kirillkitten.shikimori.data.remote.AnimeApiService
+import retrofit2.Converter
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
-@InstallIn(SingletonComponent::class)
 @Module
-object AnimeModule {
+@InstallIn(SingletonComponent::class)
+abstract class AnimeModule {
 
-    @Provides
+    @Suppress("unused")
     @Singleton
-    fun provideAnimeRepository(animeApi: AnimeApi): AnimeRepository = AnimeRepositoryImpl(animeApi)
+    @Binds
+    abstract fun bindAnimeRepository(repository: AnimeRepositoryImpl): AnimeRepository
 
-    @Provides
-    @Singleton
-    fun provideAnimeAPI(): AnimeApi = AnimeApiService
+    companion object {
 
-    @Provides
-    @Singleton
-    fun providePager(pagingConfig: PagingConfig, repository: AnimeRepository): Pager<Int, Anime> =
-        Pager(pagingConfig) {
+        @Singleton
+        @Provides
+        fun providePager(
+            pagingConfig: PagingConfig,
+            repository: AnimeRepository
+        ): Pager<Int, Anime> = Pager(pagingConfig) {
             AnimePagingSource(repository)
         }
 
-    @Provides
-    fun providePagingConfig(): PagingConfig = AnimePagingConfig
+        @Singleton
+        @Provides
+        fun provideAnimeAPI(converterFactory: Converter.Factory): AnimeApi = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(converterFactory)
+            .build()
+            .create(AnimeApi::class.java)
+
+        @Provides
+        fun provideConverterFactory(): Converter.Factory = MoshiConverterFactory.create()
+
+        @Provides
+        fun providePagingConfig(): PagingConfig = PagingConfig(pageSize = ANIME_PAGE_SIZE)
+    }
 }
