23c080a54cc2a02148ea7456fbc3fbf8e87c2b88
Kirill Kitten <khudiakovk@gmail.com>
2022-07-29 12:10:23 +0300

    add sort filter radio group

--- app/src/main/java/kirillkitten/shikimori/ui/AnimeGridScreen.kt
+++ app/src/main/java/kirillkitten/shikimori/ui/AnimeGridScreen.kt
@@ -7,18 +7,26 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.material.BottomSheetScaffold
 import androidx.compose.material.BottomSheetState
 import androidx.compose.material.BottomSheetValue
 import androidx.compose.material.Button
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.RadioButton
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.paging.compose.LazyPagingItems
@@ -123,11 +131,45 @@ fun FilterDialog() {
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp)
+            .padding(horizontal = 8.dp, vertical = 12.dp)
     ) {
         // Header
+        Text(
+            text = "Сортировать по",
+            style = MaterialTheme.typography.h6
+        )
 
         // Filters
+        // TODO Replace by string resources
+        val filterOptions = listOf("Рейтингу", "Популярности", "Алфавиту", "Дате выхода")
+        val (selectedOption, onOptionSelected) = rememberSaveable {
+            mutableStateOf(filterOptions[0])
+        }
+        Column(Modifier.selectableGroup().padding(top = 16.dp)) {
+            filterOptions.forEach { text ->
+                Row(
+                    Modifier
+                        .fillMaxWidth()
+                        .height(56.dp)
+                        .selectable(
+                            selected = (text == selectedOption),
+                            onClick = { onOptionSelected(text) },
+                            role = Role.RadioButton
+                        )
+                        .padding(horizontal = 16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    RadioButton(
+                        selected = (text == selectedOption), onClick = null
+                    )
+                    Text(
+                        text = text,
+                        style = MaterialTheme.typography.body1,
+                        modifier = Modifier.padding(start = 16.dp)
+                    )
+                }
+            }
+        }
 
         // Apply Button
         Button(
@@ -135,6 +177,7 @@ fun FilterDialog() {
                 /*TODO*/
             },
             modifier = Modifier
+                .padding(top = 16.dp)
                 .fillMaxWidth()
                 .height(48.dp)
         ) {
@@ -151,7 +194,7 @@ private fun FilterPanelPreview() {
     }
 }
 
-@Preview(showBackground = true, widthDp = 200)
+@Preview(showBackground = true)
 @Composable
 private fun FilterDialogPreview() {
     ShikimoriTheme {
